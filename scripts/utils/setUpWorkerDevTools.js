7772fab515b54613800d114e30d3d5b8906b54d2
James Lu <jamtlu@gmail.com>
2018-07-03 20:45:41 -0400

    Fix unsightly formatting on setUpWorkerDevTools.js

--- scripts/utils/setUpWorkerDevTools.js
+++ scripts/utils/setUpWorkerDevTools.js
@@ -15,6 +15,6 @@ function wrapWorkerOnMessageForAltDevTool(toWrap) {
  * Use it like this. You can include this function in your production build if you wish. Don't include altif.js in your
  * production build.
 onmessage = wrapWorkerOnMessageForAltDevTool(<your onMessage handler>)
- * The function filters out messages from the Alt Devtool, and forwards the rest to your callback.
- * If altif,js is not active, nothing will happen.
+ * The function filters out messages from Alt DevTools, and forwards the rest to your callback.
+ * If altif.js is not active, nothing will happen.
  */
