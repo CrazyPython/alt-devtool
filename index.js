f2a7f484240e760244687f9879170ec3725d1603
James Lu <jamtlu@gmail.com>
2018-07-04 19:16:27 -0400

    Now works on firefox

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-chrome.devtools.panels.create('Alt', null, 'alt-devtool.html', function (panel) { })
+try {
+    chrome.devtools.panels.create('Alt', null, 'alt-devtool.html', function (panel) {})
+} catch (error) {
+    // We may be running in firefox. Trying this other argument list instead
+    chrome.devtools.panels.create('Alt', 'assets/icon16.png', 'alt-devtool.html')
+}
